50fb3e344072db5edb4a1fca45c1eb933e284cd4
JavidNashraf <112257696+JavidNashraf@users.noreply.github.com>
2024-08-01 14:33:29 +0530

    CRUD

--- jobs/job1.py
+++ jobs/job1.py
@@ -6,17 +6,36 @@ def job1():
     cursor = conn.cursor()
     
     cursor.execute('''CREATE TABLE sample (id INTEGER PRIMARY KEY, name TEXT)''')
-    cursor.execute("INSERT INTO sample (name) VALUES ('Alice')")
-    cursor.execute("INSERT INTO sample (name) VALUES ('Bob')")
-    conn.commit()
     
-    # Query the database
-    cursor.execute("SELECT * FROM sample")
-    results = cursor.fetchall()
+    def post_job():
+        cursor.execute("INSERT INTO sample (name) VALUES ('Javid')")
+        cursor.execute("INSERT INTO sample (name) VALUES ('Nashraf')")
+        conn.commit()
+        print("POST job completed: Added Javid and Nashraf")
     
-    print("Query results:")
-    for row in results:
-        print(row)
+    def get_job():
+        cursor.execute("SELECT * FROM sample")
+        results = cursor.fetchall()
+        print("GET job results:")
+        for row in results:
+            print(row)
+    
+    def put_job():
+        cursor.execute("UPDATE sample SET name = 'UserChanged' WHERE name = 'Javid'")
+        conn.commit()
+        print("PUT job completed: Updated Javid to UserChanged")
+    
+    def delete_job():
+        cursor.execute("DELETE FROM sample WHERE name = 'Nashraf'")
+        conn.commit()
+        print("DELETE job completed: Removed Nashraf")
+    
+    post_job()
+    get_job()
+    put_job()
+    get_job()
+    delete_job()
+    get_job()
     
     # Close the connection
     conn.close()
